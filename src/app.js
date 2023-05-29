b0cde2f9b2550f646eda151758e54c709cd1bf55
dwexman <107653091+dwexman@users.noreply.github.com>
2023-05-29 21:35:14 +0000

    1er commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,14 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+var pronoun = ["the", "our", "yours"];
+var adj = ["great", "big", "excellent", "best"];
+var noun = ["jogger", "racoon", "athlete", "teacher", "programmer"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      console.log(`${pronoun[i]}${adj[j]}${noun[k]}.com`);
+    }
+  }
+}
